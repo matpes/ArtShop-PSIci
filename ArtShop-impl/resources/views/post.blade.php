8632ab2feb4c1225acd4618135b1701476b22a2a
matPes <pesicmatija1@gmail.com>
2020-05-03 16:31:07 +0200

    Izmenjeni nazivi

---
+++ ArtShop-impl/resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <h1>Post Page {{$id}}</h1>
+
+
+@stop
--- ArtShop-impl/routes/web.php
+++ ArtShop-impl/routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -12,7 +12,48 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+/*
+ Route::get('/contact', function (){
+     return "Hi I am contact";
+ });
+
+ Route::get('/about', function (){
+     return "Hi, this is about";
+ });
+
+
+ Route::get('/post/{id}/{name}', function ($id, $name) {
+
+     return "this is post number ". $id. " ". $name;
+
+
+
+
+ });
+
+
+ Route::get('admin/posts/example', array( 'as' => 'admin.home' , function () {
+
+    $url = route('admin.home');
+
+
+    return  "this url is ". $url;
+
+ }));*/
+
+
+ //Route::get('/post/{id}', 'PostController@index');
+
+
+ Route::resource('posts', 'PostController');
+
+
+ Route::get('/contact', 'PostController@contact');
+
+ Route::get('/post/{id}', 'PostController@post_some');
+
